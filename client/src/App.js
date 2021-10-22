a63e002bd3c8da72f6de6ac3c5028f2dc968a086
Akshat Dobriyal <akcooldobriyal@gmail.com>
2021-10-23 00:24:22 +0530

    Updated Navbar.js and Navbar.scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/PShare.json";
+import PShare from "./contracts/PShare.json";
+import Navbar from './components/navbar/Navbar';
+import LandingPage from './components/landingPage/LandingPage';
+import ipfsClient from 'ipfs-http-client';
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
+// Declare IPFS
+const ipfsAPI = require('ipfs-api');
+const ipfs = ipfsAPI('ipfs.infura.io', '5001', {protocol: 'https'});
+
+//const ipfsClient = require('ipfs-http-client')
+//const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }) // leaving out the arguments will default to these values
+
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      contract: null,
+      images: [],
+      loading: true
+    }
+
+    this.uploadImage = this.uploadImage.bind(this)
+    this.tipImageOwner = this.tipImageOwner.bind(this)
+    this.captureFile = this.captureFile.bind(this)
+  }
+  //state = { storageValue: 0, web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -14,18 +38,36 @@ class App extends Component {
 
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
+      console.log(accounts);
+      this.setState({ account: accounts[0] });
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+      const deployedNetwork = PShare.networks[networkId];
+      const contract = new web3.eth.Contract(
+        PShare.abi,
         deployedNetwork && deployedNetwork.address,
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract });
+
+      const imagesCount = await contract.methods.imageCount().call()
+      this.setState({ imagesCount })
+      // Load images
+      for (var i = 1; i <= imagesCount; i++) {
+        const image = await contract.methods.images(i).call()
+        this.setState({
+          images: [...this.state.images, image]
+        })
+      }
+      // Sort images. Show highest tipped images first
+      this.setState({
+        images: this.state.images.sort((a,b) => b.tipAmount - a.tipAmount )
+      })
+      this.setState({ loading: false})
+
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,7 +77,45 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
+  captureFile = event => {
+
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) })
+      console.log('buffer', this.state.buffer)
+    }
+  }
+
+  uploadImage = description => {
+    console.log("Submitting file to ipfs...")
+
+    //adding file to the IPFS
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('Ipfs result', result)
+      if(error) {
+        console.error(error)
+        return
+      }
+
+      this.setState({ loading: true })
+      this.state.contract.methods.uploadImage(result[0].hash, description).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({ loading: false })
+      })
+    })
+  }
+
+  tipImageOwner(id, tipAmount) {
+    this.setState({ loading: true })
+    this.state.contract.methods.tipImageOwner(id).send({ from: this.state.account, value: tipAmount }).on('transactionHash', (hash) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  /*runExample = async () => {
     const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
@@ -47,13 +127,14 @@ class App extends Component {
     // Update state with the result.
     this.setState({ storageValue: response });
   };
+  */
 
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
-      <div className="App">
+      /*<div className="App">
         <h1>Good to Go!</h1>
         <p>Your Truffle Box is installed and ready.</p>
         <h2>Smart Contract Example</h2>
@@ -65,6 +146,19 @@ class App extends Component {
           Try changing the value stored on <strong>line 42</strong> of App.js.
         </p>
         <div>The stored value is: {this.state.storageValue}</div>
+      </div>
+      */
+      <div>
+        <Navbar account={this.state.account} />
+        { this.state.loading
+          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
+          : <LandingPage
+              images={this.state.images}
+              captureFile={this.captureFile}
+              uploadImage={this.uploadImage}
+              tipImageOwner={this.tipImageOwner}
+            />
+        }
       </div>
     );
   }
